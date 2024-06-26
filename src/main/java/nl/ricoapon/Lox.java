574c2d2d06d717388372ce32e2386daebabd5d24
Rico Apon <rico.apon@gmail.com>
2024-06-26 13:56:19 +0000

    Initialize Maven project

---
+++ src/main/java/nl/ricoapon/Lox.java
@@ -0,0 +1,7 @@
+package nl.ricoapon;
+
+public class Lox {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/nl/ricoapon/LoxTest.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class LoxTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
